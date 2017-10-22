309314c092a84e6881cbc006a474d3ccfe3a013f
Robert Dickerson <rdickeriii@gmail.com>
2017-10-22 16:24:49 -0700

    Refactor: To meet my specs
    end_of_line

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export const Whoops404 = ({ location }) =>
+export const Whoops404 = ({location}) =>
     <div>
         <h1>Whoops, resource not found</h1>
         <p>Could not find {location.pathname}</p>
