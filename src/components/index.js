9d0af13a1aec087861858b3e612a6760f811f0b7
Robert Dickerson <rdickeriii@gmail.com>
2017-10-22 17:00:09 -0700

    Refactor: Renders the right pages
    end_of_line

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,18 @@
+import MainMenu from './ui/MainMenu'
+
+export const Left = ({children}) =>
+    <div className='page'>
+        <MainMenu calssName='main-menu'/>
+        {children}
+    </div>;
+export const Right = ({children}) =>
+    <div className='page'>
+        {children}
+        <MainMenu calssName='main-menu'/>
+    </div>;
+
 export const Whoops404 = ({location}) =>
     <div>
         <h1>Whoops, resource not found</h1>
         <p>Could not find {location.pathname}</p>
-    </div>
+    </div>;
